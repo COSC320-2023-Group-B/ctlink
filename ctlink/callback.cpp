4fcafa1ee559d553411ba1a317b16d3a18cc8c5b
Fletcher Moloney <fletcherhogarth@gmail.com>
2023-08-10 17:24:23 +1000

    Added debug printing in callback.cpp functions

--- ctlink/callback.cpp
+++ ctlink/callback.cpp
@@ -6,19 +6,27 @@ void LIBCTAPI on_device_discovered(libct_context_t* context, libct_device_t* dev
 	const char* address = libct_device_get_address(device);
 	const char* name = libct_device_get_name(device);
 	// do something
+
+	std::cout << "on_device_discovered Called";
 }
 
 void LIBCTAPI on_device_connected_ready(libct_context_t* context, libct_device_t* device)
 {
 	// do something
+
+	std::cout << "on_device_connected_ready Called";
 }
 
 void LIBCTAPI on_device_disconnected(libct_context_t* context, libct_device_t* device)
 {
 	// do something
+
+	std::cout << "on_device_disconnected Called";
 }
 
 void LIBCTAPI on_data_received(libct_context_t* context, libct_device_t* device, libct_stream_data_t* data)
 {
 	// handle data
+
+	std::cout << "on_data_received Called";
 }
